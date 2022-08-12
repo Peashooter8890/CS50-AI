cfb13ee77d464a72ae3232a3db29342c08fd0a1d
Mark Bu <peashooterking8@gmail.com>
2022-08-11 21:31:57 -0400

    Finished Knights

--- project1/knights/puzzle.py
+++ project1/knights/puzzle.py
@@ -1,3 +1,4 @@
+from re import M
 from logic import *
 
 AKnight = Symbol("A is a Knight")
@@ -12,21 +13,30 @@ CKnave = Symbol("C is a Knave")
 # Puzzle 0
 # A says "I am both a knight and a knave."
 knowledge0 = And(
-    # TODO
+    Or(AKnight,AKnave),
+    Implication(AKnight, And(AKnight,AKnave))
 )
 
 # Puzzle 1
 # A says "We are both knaves."
 # B says nothing.
 knowledge1 = And(
-    # TODO
+    Or(AKnight,AKnave),
+    Or(BKnight,BKnave),
+    Implication(AKnight, And(AKnave,BKnave)),
+    Implication(AKnave, Not(And(AKnave,BKnave)))
 )
 
 # Puzzle 2
 # A says "We are the same kind."
 # B says "We are of different kinds."
 knowledge2 = And(
-    # TODO
+    Or(AKnight,AKnave),
+    Or(BKnight,BKnave),
+    Implication(AKnight,BKnight),
+    Implication(AKnave,BKnight),
+    Implication(BKnight,AKnave),
+    Implication(BKnave,AKnave)
 )
 
 # Puzzle 3
@@ -35,7 +45,19 @@ knowledge2 = And(
 # B says "C is a knave."
 # C says "A is a knight."
 knowledge3 = And(
-    # TODO
+    Or(AKnight,AKnave),
+    Or(BKnight,BKnave),
+    Or(CKnight,CKnave),
+
+    Not(And(AKnight,AKnave)),
+    Not(And(BKnight,BKnave)),
+    Not(And(CKnight,CKnave)),
+    Implication(AKnight,Or(AKnight,AKnave)),
+    Implication(AKnave,Not(Or(AKnight,AKnave))),
+    Implication(BKnight,Or(AKnight,AKnave)),
+    Biconditional(BKnight,CKnave),
+    Biconditional(CKnight,AKnight)
+
 )
 
 
